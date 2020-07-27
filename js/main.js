9d66e1483eab145a6a538957c80a08bee49dbdbf
Andresrom1123 <amclres@gmail.com>
2020-07-26 20:46:46 -0500

    added a new practice

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 Vue.component('card', {
-  props: ['title', 'description', 'image', 'url'],
+  props: ['title', 'description', 'image', 'url', 'codigo'],
   template: `
 <div class="col-sm-12 col-lg-4 mb-5">
   <div class="border border-top-0 marco">
     <div class="scroll" style="overflow: auto;  height: 200px" >
       <img  class="" :src="image" width="100%"  ></img>
     </div>
+    <p class="text-center text-info mb-0">{{ codigo }}</p>
     <h5 class="pl-3 pt-2 verde">{{title}}</h5>
     <div class="" style="height: 160px; overflow: auto;">
       <p class="mt-3  pl-3 pr-3">{{ description }}</p>
@@ -26,12 +27,14 @@ new Vue({
       { 
         id: 1,
         title: 'Expert Answers(Desarrollando)',
+        codigo: 'Vue - Django',
         description: 'Una red social de preguntas y respuestas que en un primer contacto te hace recordar a Yahoo! Respuesta',
         url: 'https://dev-expert-answers.netlify.app/',
         image: '/imagenes/expert-answer.png'
       },
       { 
         id: 2,
+        codigo: 'Vue - Django',
         title: 'Newsletters(Desarrollando)',
         description: 'Interfaz para que los usuarios puedan suscribirse al boletín de su preferencia, también podrán votar para que se agreguen nuevos boletines',
         url: 'https://awesome-visvesvaraya-2565c3.netlify.app/',
@@ -42,9 +45,18 @@ new Vue({
       { 
         id: 1,
         title: 'Tarjeta Trello',
-        description: 'Practica de una tarjeta creada con vue y html',
+        codigo: 'Vue js - Html',
+        description: 'Practica de una tarjeta similares a las de trello creada con vue y html',
         url: 'https://vibrant-bose-3bdd9c.netlify.app/',
         image: 'https://353a23c500dde3b2ad58-c49fe7e7355d384845270f4a7a0a7aa1.ssl.cf2.rackcdn.com/5d71827232c7d1018dc87779/screenshot.png'
+      },
+      {
+        id: 2,
+        codigo: 'Html - Css',
+        title: 'Banner Netflix La Casa de Papel',
+        description: 'Practica de un Banner de Netflix',
+        url: 'https://cdpn.io/AndresRomero/debug/zgEMxj/WPMLYZXmazGr',
+        image: '/imagenes/banner-casa-de-papel.png',
       }
     ],
   }
